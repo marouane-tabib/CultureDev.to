454629ce82c3969f2c89548a5b724f30f8c2a19e
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-16 12:01:49 +0100

    add jquery cdn

--- login.php
+++ login.php
@@ -10,6 +10,7 @@
 </head>
 <body>
     <div></div>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
