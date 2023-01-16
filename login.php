df7abcac2521c7902fe9cf27a23b831819aa44ec
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-16 14:04:20 +0100

    add main.css link stylesheet in login.php file

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
     <title>Sign In</title>
 </head>
 <body>
