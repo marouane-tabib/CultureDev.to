c871cc965987a659a72be99f4232236383c5a2b1
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-16 15:20:12 +0100

    add header and footer layouts

---
+++ layouts/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Links -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <!-- End Links -->
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <title>Sign In</title>
+</head>
+<body>
+    <?php include'../components/nav-bar.php'; ?>
