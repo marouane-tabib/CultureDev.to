3af8afb86aedb0fe72989352fe64856000d1fc2e
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-16 15:32:28 +0100

    add deynamic title

--- layouts/header.php
+++ layouts/header.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <!-- End Links -->
     <link rel="stylesheet" href="../assets/css/main.css">
-    <title>Sign In</title>
+    <title><?php if(isset($title)){ echo $title; }else{ echo "Home";} ?></title>
 </head>
 <body>
     <?php include'../components/nav-bar.php'; ?>
