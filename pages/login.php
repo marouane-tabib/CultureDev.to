8db011d0e8d2deee76201ec348e14c83360be4fe
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-16 15:36:34 +0100

    add deynamic layouts in register and login pages - refactoring

--- pages/login.php
+++ pages/login.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/main.css">
-    <title>Sign In</title>
-</head>
-<body>
+<?php $title = "Log In" ?>
+<?php include'../layouts/header.php'; ?>
     <div class="data-form-box bg-white mx-auto mt-5 col-md-5 col-11">
         <h3>Log In</h3><hr>
         <form>
@@ -27,7 +17,4 @@
             </div>
         </form>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php include'../layouts/footer.php'; ?>
