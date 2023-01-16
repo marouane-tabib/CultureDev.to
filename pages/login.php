7a8ce917be148486c5d2fbaab3d13fe4150a2a10
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-16 14:44:59 +0100

    add data-forms.scss file in components folder , and like it with login and logout pages

--- pages/login.php
+++ pages/login.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
     <title>Sign In</title>
 </head>
 <body>
-    <div class="login-box bg-white mx-auto mt-5 col-md-5 col-11">
+    <div class="data-form-box bg-white mx-auto mt-5 col-md-5 col-11">
         <h3>Log In</h3><hr>
         <form>
             <div class="mb-3">
--- pages/logout.php
+++ pages/logout.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
     <title>Log Out</title>
 </head>
 <body>
-    <div class="login-box bg-white mx-auto mt-5 col-md-5 col-11">
+    <div class="data-form-box bg-white mx-auto mt-5 col-md-5 col-11">
         <h3>Log Out</h3><hr>
         <form>
             <div class="row">
@@ -31,7 +31,7 @@
                 <label for="password" class="form-label">Password</label>
                 <input type="password" class="form-control" id="password">
             </div>
-            <div class="form-text mb-3">You Need an Account ? <a href="logout.php">Log Out </a>Now</div>
+            <div class="form-text mb-3">You Ready Create an Account ? <a href="login.php">Log In </a>Now</div>
             <div class="btn-box">
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
