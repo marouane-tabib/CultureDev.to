2dfe1aeca2d0bb6233c17cf7e5f5aa6599fcd1af
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-18 16:27:27 +0100

    add names and placeholder , validate min and max lenght in inputs

--- pages/login.php
+++ pages/login.php
@@ -2,20 +2,19 @@
 <?php 
     include '../classes/User.php';
     $user = new User();
-
-    $user->login("admin" , "admin");
+    $user->login("adminf" , "admin");
 ?>
 <?php include'../layouts/header.php'; ?>
     <div class="data-form-box bg-white mx-auto mt-5 col-md-6 col-12">
         <h1>Log In</h1><hr>
         <form action="POST" method="login.php">
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="username" class="form-label">User Name</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Add Your User Name" minlength="4" maxlength="50">
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="********" minlength="8" maxlength="20">
             </div>
             <div class="form-text mb-3">You Need an Account ? <a href="register.php">Register</a> Now</div>
             <div class="btn-box">
