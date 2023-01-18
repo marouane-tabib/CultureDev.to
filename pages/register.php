b5e545b59463ce14227ad12e11bb87dde3653f1d
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-18 18:58:31 +0100

    add register method , and data form attr - completed

--- pages/register.php
+++ pages/register.php
@@ -1,29 +1,40 @@
 <?php $title = "Register" ; $fade = true?>
+<?php 
+    include '../classes/User.php';
+    $user = new User();
+    if(isset($_POST['btn'])){
+        $user->register($_POST['first_name'] , $_POST['last_name'] , $_POST['user_name'] , $_POST['password'] , $_POST['confirm_password']);
+    }
+?>
 <?php include'../layouts/header.php'; ?>
     <div class="data-form-box bg-white mx-auto mt-5 col-md-6 col-11">
         <h1>Register</h1><hr>
-        <form>
+        <form action="register.php" method="POST">
             <div class="row">
                 <div class="mb-3 col">
                     <label for="firstName" class="form-label">First Name</label>
-                    <input type="text" class="form-control" id="firstName">
+                    <input type="text" class="form-control" id="firstName" name="first_name" placeholder="First Name">
                 </div>
                 <div class="mb-3 col">
                     <label for="lastName" class="form-label">Last Name</label>
-                    <input type="text" class="form-control" id="lastName" >
+                    <input type="text" class="form-control" id="lastName" name="last_name" placeholder="Add Your Last Name">
                 </div>
             </div>
             <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                <label for="username" class="form-label">User Name</label>
+                <input type="text" class="form-control" id="username" name="user_name" placeholder="Add Your User Name">
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="*******">
+            </div>
+            <div class="mb-3">
+                <label for="confirmPassword" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="confirmPassword" name="confirm_password" placeholder="*******">
             </div>
             <div class="form-text mb-3">You Ready Create an Account ? <a href="login.php">Log In </a>Now</div>
             <div class="btn-box">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary" name="btn">Submit</button>
             </div>
         </form>
     </div>
