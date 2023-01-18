d736b8dbb75e058c24a7674cbda70640e65a7a45
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-18 16:23:14 +0100

    add login error component

--- layouts/header.php
+++ layouts/header.php
@@ -15,3 +15,13 @@
     <?php if(!isset($fade)){ ?>
     <?php include'../components/nav-bar.php'; ?>
     <?php } ?>
+            <?php if (isset($_SESSION['action'])): ?>
+            <div class="container <?php echo $_SESSION['action']['class'] ?>" role="alert">
+              <strong><?php echo $_SESSION['action']['status'] ?></strong>
+                <?php 
+                  echo $_SESSION['action']['message']; 
+                  unset($_SESSION['action']);
+                ?>
+                  <button type="button" class="btn-close" data-bs-dismiss="alert"></span>
+              </div>
+            <?php endif ?>
