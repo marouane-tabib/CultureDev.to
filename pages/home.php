f25c1be9855f885519b9837fb032e8f6ed67de86
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-19 11:12:18 +0100

    BC-2 add static table in home.php file

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,23 @@
 <?php include'../layouts/header.php'; ?>
-    <h1>Home Page</h1>
+<section class="container">
+    <h1>Index Page</h1>
+    <table class="table bg-white">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+    </table>
+</section>
 <?php include'../layouts/footer.php'; ?>
