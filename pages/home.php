8161524b70e62f3230920992c58c059d6252c32c
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-19 13:54:50 +0100

    add table in components/home folder and include it in home.php file

--- pages/home.php
+++ pages/home.php
@@ -2,26 +2,10 @@
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
 <section class="container">
     <h1>Index Page</h1>
-    <?php include '../components/home/modal.php' ?>
-
-    <table class="table bg-white">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-        </tbody>
-    </table>
+    <?php 
+        include '../components/home/modal.php';
+        include '../components/home/table.php';
+    ?>
 </section>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
