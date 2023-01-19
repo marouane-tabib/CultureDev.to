1da18d482a6ac3020ce655dece5b4b6857d2c5d7
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-19 16:53:06 +0100

    BC-2 create add Article method - completed

--- classes/Article.php
+++ classes/Article.php
@@ -1,9 +1,17 @@
 <?php 
     include 'database.php';
-    class User extends database{
+    class Article extends database{
 
-        public function add(){
-            // 
+        public function add($title , $category_id , $description){
+            $title = htmlspecialchars(strip_tags($title));
+            $category = htmlspecialchars(strip_tags($category_id));
+            $description = htmlspecialchars(strip_tags($description));
+
+            $a = new database();
+            $a->insert('articles',['title'=>$category,'category_id'=>$category_id,'description'=>$description , 'date' => date('d-m-y h:i:s')]);
+            if ($a == true) {
+                echo "article added";
+            }
         }
 
     }
