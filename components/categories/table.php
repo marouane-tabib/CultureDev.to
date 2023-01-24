3bfaed58cf20362086e0919c4a5ba6a5bc7a6c22
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 15:25:20 +0100

    add categories table

---
+++ components/categories/table.php
@@ -0,0 +1,20 @@
+
+    <table class="table bg-white" id="myTable">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">name</th>
+            <th scope="col">action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Sport</td>
+            <td>
+                <a href="#">Delete</a>
+                <a href="#">Update</a>
+            </td>
+            </tr>
+        </tbody>
+    </table>
