1f9a7ae31011da738d67dfa29bc35860d6c326ff
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 11:15:53 +0100

    add data bale CDN

--- layouts/footer.php
+++ layouts/footer.php
@@ -2,6 +2,7 @@
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <!-- End Scripts -->
 </body>
 </html>
--- layouts/header.php
+++ layouts/header.php
@@ -7,6 +7,7 @@
     <!-- Links -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
     <!-- End Links -->
     <link rel="stylesheet" href="../assets/css/main.css">
     <title><?php if(isset($title)){ echo $title; }else{ echo "Home";} ?></title>
