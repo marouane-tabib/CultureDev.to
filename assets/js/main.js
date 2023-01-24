f2135195d6f5289c44f0092498833f4afe51c135
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 11:25:59 +0100

    clean

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 // Summernote.org
     $('#summernote').summernote({
-        // placeholder: 'Hello stand alone ui',
         tabsize: 2,
         height: 120,
         toolbar: [
@@ -13,16 +12,14 @@
 // Bootstrap.com/accordion
     let articleForm = $('.accordion').html(),
     i = 0;
-
     // $('#title').on('keyup' , function(){
     //   let titleVal = $(this).val();
     //   $('#articleTitle').text(titleVal);
     // });
-
     $('#add-article-form').on('click' , function(){
-    i++;
-    $('#article-form').after($('#article-form').html()).find('#articleTitle').attr('data-bs-target', '#' + i);
-    $('#article-form').find('.accordion-collapse').attr({'id': parseInt(i)});
+        i++;
+        $('#article-form').after($('#article-form').html()).find('#articleTitle').attr('data-bs-target', '#' + i);
+        $('#article-form').find('.accordion-collapse').attr({'id': parseInt(i)});
     });
 // datatables.net
     $(document).ready( function () {
