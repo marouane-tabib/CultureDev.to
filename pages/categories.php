7f32b6f59c1be7b74ec912080aaad7dcbdcd65e3
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 15:19:11 +0100

    add layouts structure in categories.php

---
+++ pages/categories.php
@@ -0,0 +1,9 @@
+<?php include '../layouts/header.php'; ?>
+<?php 
+    include '../classes/Article.php';
+?>
+<section class="container">
+    <h1>Category Index</h1>
+    
+</section>
+<?php include '../layouts/footer.php'; ?>
