c9248345fc461ee9ee09cda29072d3fc8c48d11f
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 15:15:55 +0100

    add category class

---
+++ classes/Category.php
@@ -0,0 +1,26 @@
+<?php 
+    include 'database.php';
+    class Category extends database{
+        
+        public function show(){
+            // 
+        }
+
+        public function sessionGenerator($status = "success" , $message = "WELCOME BACK ! Login Success ... "){
+            if ($status == "success") {
+                $_SESSION['action'] = [
+                    'status' => "Success !",
+                    'class' => "alert alert-success alert-dismissible fade show",
+                    'message' => $message,
+                ];
+            }elseif($status == "issue"){
+                $_SESSION['action'] = [
+                    'status' => "Problem !",
+                    'class' => "alert alert-danger alert-dismissible fade show",
+                    'message' => $message,
+                ];
+            }
+        }
+
+    }
+?>
