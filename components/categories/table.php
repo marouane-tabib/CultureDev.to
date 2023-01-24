e8abccb5dcaf49665e7bfefb1e2de481a1a05cc6
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 15:42:38 +0100

    BC-2 show categories method - completed

--- components/categories/table.php
+++ components/categories/table.php
@@ -8,13 +8,15 @@
             </tr>
         </thead>
         <tbody>
+            <?php while ($row = mysqli_fetch_assoc($result)) { ?>
             <tr>
-            <th scope="row">1</th>
-            <td>Sport</td>
-            <td>
-                <a href="#">Delete</a>
-                <a href="#">Update</a>
-            </td>
+                <th scope="row">1</th>
+                <td><?= $row['name'] ?></td>
+                <td>
+                    <a href="<?= $row['id'] ?>">Delete</a>
+                    <a href="#">Update</a>
+                </td>
             </tr>
+            <?php } ?>
         </tbody>
     </table>
--- pages/categories.php
+++ pages/categories.php
@@ -1,6 +1,8 @@
 <?php include '../layouts/header.php'; ?>
 <?php 
-    include '../classes/Article.php';
+    include '../classes/Category.php';
+    $category = new Category();
+    $result = $category->show();
 ?>
 <section class="container">
     <h1>Category Index</h1>
