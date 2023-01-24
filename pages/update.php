b86eefbe065d9ead2bb625c1d9d6bcda353527bd
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 13:55:10 +0100

    add layouts structre in update.php

---
+++ pages/update.php
@@ -0,0 +1,8 @@
+<?php include '../layouts/header.php'; ?>
+<?php 
+    include '../classes/Article.php';
+?>
+<section class="container">
+    <h1>Update Article</h1>
+</section>
+<?php include '../layouts/footer.php'; ?>
