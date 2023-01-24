7a21edcc3d9edb399dd85c0a151e6ed9fca883d1
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 15:20:26 +0100

    add categories and articles links to nav-bar

--- components/nav-bar.php
+++ components/nav-bar.php
@@ -8,10 +8,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                <a class="nav-link" href="#">Posts</a>
+                <a class="nav-link" href="home.php">Articles</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" href="#">Catégories</a>
+                <a class="nav-link" href="categories.php">Categories</a>
                 </li>
             </ul>
             </div>
