5c8f18b6cb45a032ca583088fbdad85775497004
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-24 11:25:18 +0100

    add all scripts in main.js file

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// Summernote.org
+    $('#summernote').summernote({
+        // placeholder: 'Hello stand alone ui',
+        tabsize: 2,
+        height: 120,
+        toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ]
+    });
+// Bootstrap.com/accordion
+    let articleForm = $('.accordion').html(),
+    i = 0;
+
+    // $('#title').on('keyup' , function(){
+    //   let titleVal = $(this).val();
+    //   $('#articleTitle').text(titleVal);
+    // });
+
+    $('#add-article-form').on('click' , function(){
+    i++;
+    $('#article-form').after($('#article-form').html()).find('#articleTitle').attr('data-bs-target', '#' + i);
+    $('#article-form').find('.accordion-collapse').attr({'id': parseInt(i)});
+    });
+// datatables.net
+    $(document).ready( function () {
+        $('#myTable').DataTable();
+    });
--- pages/home.php
+++ pages/home.php
@@ -24,41 +24,4 @@
 </section>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
-    <script>
-      $('#summernote').summernote({
-        // placeholder: 'Hello stand alone ui',
-        tabsize: 2,
-        height: 120,
-        toolbar: [
-          ['style', ['style']],
-          ['font', ['bold', 'underline', 'clear']],
-          ['color', ['color']],
-          ['para', ['ul', 'ol', 'paragraph']],
-        ]
-      });
-    </script>
-<script>
-  let articleForm = $('.accordion').html(),
-      i = 0;
-
-  // $('#title').on('keyup' , function(){
-  //   let titleVal = $(this).val();
-  //   $('#articleTitle').text(titleVal);
-  // });
-  
-  $('#add-article-form').on('click' , function(){
-    i++;
-    $('#article-form').after($('#article-form').html()).find('#articleTitle').attr('data-bs-target', '#' + i);
-    $('#article-form').find('.accordion-collapse').attr({'id': parseInt(i)});
-    // $('#article-form').find('#title').on('keyup' , function(){
-    //   let titleVal = $(this).val();
-    //   $('#articleTitle').text(titleVal);
-    // });
-  });
-</script>
-<script>
-  $(document).ready( function () {
-    $('#myTable').DataTable();
-  });
-</script>
 <?php include '../layouts/footer.php'; ?>
