3daffe2b22908333773938dc7c89bc65cc93c4d8
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-25 11:15:08 +0100

    clean

--- pages/home.php
+++ pages/home.php
@@ -1,24 +1,25 @@
-<?php include '../layouts/header.php'; ?>
-<?php 
-    include '../classes/Article.php';
-    include '../classes/User.php';
+<?php include '../layouts/header.php';include '../classes/Article.php';include '../classes/User.php'; 
     // User Object
       $user = new User();
       $sumUsers = $user->sumUsers();
     // End
-    $article = new Article();
-    $articles = $article->show();
-    $categoryResult = $article->showCategories();
-    if(isset($_POST['add'])){
-      for ($i=0; $i < count($_POST['title']); $i++) { 
-        $article->add($_POST["title"][$i] , $_POST["category"][$i] , $_POST['description'][$i]);
+    // Articles Object
+      $article = new Article();
+      $articles = $article->show();
+      $categoryResult = $article->showCategories();
+      // Add Article
+      if(isset($_POST['add'])){
+        for ($i=0; $i < count($_POST['title']); $i++) { 
+          $article->add($_POST["title"][$i] , $_POST["category"][$i] , $_POST['description'][$i]);
+        }
+        header('location:home.php');
       }
-      header('location:home.php');
-    }
-    if(isset($_POST['deleteArticle'])){
-      $result = $article->destroy($_POST['deleteArticle']);
-      header('location:home.php');
-     }
+      // Delete Article
+      if(isset($_POST['deleteArticle'])){
+        $result = $article->destroy($_POST['deleteArticle']);
+        header('location:home.php');
+      }
+    // End
     if(!isset($_SESSION['user_info'])){
       header('location:login.php');
     }
