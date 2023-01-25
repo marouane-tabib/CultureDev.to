6672846c7096be9535e5137fd4fd201cca13afb8
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-25 11:17:14 +0100

    BC-4 add statistic cards in components folder

---
+++ components/home/statistic_cards.php
@@ -0,0 +1,15 @@
+
+    <div class="row mx-auto">
+      <div class="col-11 col-md main-style px-3 py-2 mx-1">
+        Articles
+        <h1 class="float-end"><?= $articles->num_rows ?></h1>
+      </div>
+      <div class="col-11 col-md main-style px-3 py-2 mx-1">
+        Categories
+        <h1 class="float-end"><?= $categoryResult->num_rows ?></h1>
+      </div>
+      <div class="col-11 col-md main-style px-3 py-2 mx-1">
+        Users
+        <h1 class="float-end"><?= $sumUsers['sum_users'] ?></h1>
+      </div>
+    </div>
--- pages/home.php
+++ pages/home.php
@@ -26,21 +26,8 @@
 ?>
 <section class="container">
     <h1>Index Page</h1>
-    <div class="row mx-auto">
-      <div class="col-11 col-md main-style px-3 py-2 mx-1">
-        Articles
-        <h1 class="float-end"><?= $articles->num_rows ?></h1>
-      </div>
-      <div class="col-11 col-md main-style px-3 py-2 mx-1">
-        Categories
-        <h1 class="float-end"><?= $categoryResult->num_rows ?></h1>
-      </div>
-      <div class="col-11 col-md main-style px-3 py-2 mx-1">
-        Users
-        <h1 class="float-end"><?= $sumUsers['sum_users'] ?></h1>
-      </div>
-    </div>
     <?php 
+        include '../components/home/statistic_cards.php';
         include '../components/home/modal.php';
         include '../components/home/table.php';
     ?>
